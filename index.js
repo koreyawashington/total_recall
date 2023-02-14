5829a57682043c63b16d0007bd985f2d6bad80fb
koreyawashington <122809994+koreyawashington@users.noreply.github.com>
2023-02-14 10:48:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,107 @@
+// Example of What your JS File in VS Code should look like
+
+
+/*
+	Remember!!! Once you’ve completed a problem,
+	COMMENT OUT YOUR WORK before moving on to the next one.
+	This makes it easier to read and debug the current 
+	Solution you are working on.
+
+    GitHub Help
+
+As a refresher to what we are doing:
+
+git init <- initiates a hidden folder in your files to use as a staging area that will be pushed to the cloud
+
+The three lines of code copied connect that folder created to the cloud. 
+
+Afterwards, the only lines of code you SHOULD need are:
+git add . <-- the period adds all files in the folder to the staging area.
+git commit -m "message" <-- This is 'm' is for message that you type in quotations showing what work was done,. 
+
+git push <--- finalizes and 'pushes' the code to github
+
+
+
+Once you finish your lab/HW remember to push to github:
+
+Git add . 
+Git commit -m”final commit”
+Git push
+
+*/
+
+
+// I. Variables & Data Types
+// A. Q + A
+// B. Strings
+// C. Booleans
+// D. The Farm
+// E. Driver's Ed
+//_____________________________________________________________
+
+
+
+
+
+
+// II. Loops
+// A. The Basics
+// B. Get even
+// C. Give me Five
+// D. Savings account
+//_____________________________________________________________
+
+
+
+
+
+
+
+
+// III. Arrays & Control Flow
+// A. Talk about it:
+// B. Easy Does It
+// C. Accessing elements
+// D. Change values
+// E. Mix It Up
+// F. Biggie Smalls
+// G. Monkey in the Middle
+// H. What's in Your Closet?
+//_____________________________________________________________
+
+
+
+
+
+
+
+
+// IV. Functions
+// A.
+// B.
+// C.
+// D.
+// E.
+// F.
+// G.
+// H.
+//_____________________________________________________________
+
+
+
+
+
+
+// V. Objects
+// A.
+// B.
+// C.
+// D.
+// E.
+// F.
+// G.
+//_____________________________________________________________
+
+
+// Extra
