d96d8e84df0a8062245084fb276d074b492a1e71
koreyawashington <122809994+koreyawashington@users.noreply.github.com>
2023-02-20 20:48:58 -0500

    dayThreeTotalRecall

--- indexThird.js
+++ indexThird.js
@@ -1,129 +1,157 @@
 // Creating Classes & Factories
 /////////////////////////////////////
-// Created Hamster Class
+// Hamster Class created
 // //////////////////////////////////////
 
-// class Hamster{
-//     constructor(owner,name,price){
-//         this.owner = owner
-//         this.name = name
-//         this.price = price
-//     }
+class Hamster{
+    constructor(owner,name,price){
+        this.owner = owner
+        this.name = name
+        this.price = price
+    }
 
-// wheelRun(){
-//     console.log("squeak squeak");
-// }eatFood(){
-//     console.log("nibble nibble");
-// }getPrice(){
-//     console.log(this.price)
-// }
+wheelRun(){
+    console.log("squeak squeak");
+}eatFood(){
+    console.log("nibble nibble");
+}getPrice(){
+    return this.price
+}
 
-// }
-//  let pet1 = new Hamster("Kody","Panther",18);
-//  let pet2 = new Hamster("Mary","Brownie",15);
+}
+ let pet1 = new Hamster("Kody","Panther","Eighteen Dollars");
+ let pet2 = new Hamster("Mary","Brownie","Twelve Dollars");
 
 
  
-//  console.log(pet2.owner + " pet's name is , " + pet2.name + " and she was purchased for " + pet2.price + " dollars.");
-//  console.log(pet1.name + " is being rehomed to " + pet2.owner +"'s family.");
-// //  
+ console.log(pet2.owner + " pet's name is , " + pet2.name + " and she was purchased for " + pet2.price + " dollars.");
+ console.log(pet1.name + " is being rehomed to " + pet2.owner +"'s family.");
+//  
 
 
 
-////////////////////////////////////////
-// Created Person class
-/////////////////////////////////////
+// ////////////////////////////////////////
+// //Person class created
+// /////////////////////////////////////
+
 class Person {
-
-    constructor(name,age, height, weight, mood,hamsters, bankAccount){
-this.name = name
-this.age = age;
-this.height = height;
-this.weight = weight;
-this.mood = mood;
-this.hamsters = hamsters;
-this.bankAccount = bankAccount;
-
-    }getName(){
-        console.log(this.name);
-    }getAge(){
-        return this.age;
-    }getWeight(){
-        return this.weight
-    }greet(){
-        console.log();
-    }eat(){
-
-    }exercise(){
-
-    }ageUp(){
-
-    }buyhamster(){
-
+    constructor(name) {
+        this.name = name
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamsters = []
+        this.bankAccount = 0
     }
+
+    getName() {
+        return this.name
+    }
+    getAge() {
+        return this.age
+    }
+    getWeight() {
+        return this.weight
+    }
+    greet() {
+        console.log(`Mucho Gusto! I am ${this.name} greeting.`)
+    }
+    eat() {
+        this.weight++
+        this.mood++
+    }
+    exercise() {
+        this.weight--
+    }
+
+    ageUp() {
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount += 10
+    }
+    buyHamster(hamster) {
+        this.hamsters.push(hamster)
+        this.mood += 10
+        this.bankAccount -= hamster.getPrice()
+    }
+
 }
 
-// Create a Story with your Person class
-// Feel free to update or add methods to automate some of these tasks.
+// //////////////////////////////////////////////
+// // Create a Story with your Person class
+// // Timmy's story created
+// /////////////////////////////////////////////
 
-// Instantiate a new Person named Timmy
-// Age Timmy five years
-// At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
-// Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times
-// Age Timmy 9 years
-// Create a hamster named "Gus"
-// Set Gus's owner to the string "Timmy"
-// Have Timmy "buy" Gus
-// Age Timmy 15 years
-// Have Timmy eat twice
-// Have Timmy exercise twice
-// 🔴 Hard Mode Save & Commit your work!
-// Your commit message should read something like:
-// "created timmys story" Easy Mode Keep Going Save and Run Code
+const timmy = new Person('Timmy');
 
-// const timmy = new Person('Timmy')
-// for(let i =0; i < 5; i++){
-//   timmy.ageUp()
-// }
-// for(let i =0; i < 5; i++){
-//   timmy.eat()
-// }
-// for(let i =0; i < 5; i++){
-//   timmy.exercise()
-// }
-// for(let i =0; i < 9; i++){
-//   timmy.ageUp()
-// }
-// const gus = new Hamster('Gus')
-// gus.owner = 'Timmy'
-// timmy.buyHamster(gus)
+for (let i = 0; i < 5; i++) {
+  timmy.ageUp();
+}
 
-// for(let i =0; i < 15; i++){
-//   timmy.ageUp()
-// }
-// timmy.eat()
-// timmy.eat()
-// timmy.exercise()
-// timmy.exercise()
-// Chef Make Dinners
-// class Dinner {
+for (let i = 0; i < 5; i++) {
+  timmy.eat();
+}
 
-// }
+for (let i = 0; i < 5; i++) {
+  timmy.exercise();
+}
 
-// class Chef {
+for (let i = 0; i < 9; i++) {
+  timmy.ageUp();
+}
 
-// }
-// Chef should be a factory of Dinner
-// Add a constructor to dinner that sets the string properties, appetizer, entree and dessert.
-// Add a method on chef that takes three arguments and returns a new Dinner based on those arguments.
-// Have the Chef create 3 dinners, log the dinners
-// The only thing important here is that student recognizes that the Chef should produce dinners
-// No wrong answers as long as the Chef can produce dinners
-// Chef should have an array of dinners
-// Part 1 is about following instructions, part 2 is purposefully ambiguous, as a dev you need to be able to do both
-// 🔴 Hard Mode Save & Commit your work!
-// Your commit message should read something like:
-// "Dinner is served" Easy Mode Keep Going Save and Run Your Code
+const gus = new Hamster('Timmy','Gus','Twenty-Five Dollars');
+gus.owner = "Timmy"
+
+timmy.buyHamster(gus);
+
+for (let i = 0; i < 15; i++) {
+  timmy.ageUp();
+}
+
+timmy.eat();
+timmy.eat();
+
+timmy.exercise();
+timmy.exercise()
+console.log(gus);
+console.log(timmy);
+
+
+////////////////////////////////////
+// dinner is served
+////////////////////////////////////
+
+class Dinner {
+      constructor(appetizer, entree, dessert) {
+        this.appetizer = appetizer;
+        this.entree = entree;
+        this.dessert = dessert;
+      }
+    }
+    
+    class Chef {
+      getDinner(appetizer, entree, dessert) {
+        const dinner = new Dinner(appetizer, entree, dessert);
+        return dinner;
+      }
+    }
+    
+    const chef1 = new Chef();
+    
+    const dinnersOfChef = [];
+    const dinner1 = chef1.getDinner('French Fries', 'Vegan Burger', 'Peanut Butter Cookies');
+    const dinner2 = chef1.getDinner('Tortilla Chips with Nacho Cheese and Salsa', 'Quesodillas', 'Cinnamon Twists');
+    const dinner3 = chef1.getDinner('Chicken Wings', 'Pizza', 'Jalepeno Poppers');
+    dinnersOfChef.push(dinner1);
+    dinnersOfChef.push(dinner2);
+    dinnersOfChef.push(dinner3);
+    console.log(dinnersOfChef);
+    
+
 
 // I. Variables & Datatypes
 // To answer these questions, you can add them in a multiline comment section inside of script.js
